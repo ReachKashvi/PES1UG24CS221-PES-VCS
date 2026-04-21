efb5748e8eb12612dc9e78d43803b71a1e0b50a8
ReachKashvi <reachkashvi.m@gmail.com>
2026-04-21 23:26:20 +0530

    Phase 4: Implement tree generation for commit_create

--- commit.c
+++ commit.c
@@ -194,8 +194,14 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
+    Commit commit = {0};
+    
+    // Step 1: Create the tree from the current index
+    if (tree_from_index(&commit.tree) != 0) {
+        return -1;
+    }
+    
+    // Remaining steps will be implemented in subsequent commits
     (void)message; (void)commit_id_out;
     return -1;
 }
