ab0c0562ca1d8d1bc73664a4bcab6faaf687fd72
ReachKashvi <reachkashvi.m@gmail.com>
2026-04-21 16:16:58 +0530

    Phase 1: Implement header creation in object_write

--- object.c
+++ object.c
@@ -94,8 +94,16 @@ int object_exists(const ObjectID *id) {
 //
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
-    // TODO: Implement
-    (void)type; (void)data; (void)len; (void)id_out;
+    char type_str[16];
+    if (type == OBJ_BLOB) strcpy(type_str, "blob");
+    else if (type == OBJ_TREE) strcpy(type_str, "tree");
+    else strcpy(type_str, "commit");
+
+    char header[64];
+    int hlen = sprintf(header, "%s %zu", type_str, len) + 1;
+
+    // Remaining logic will be implemented in next commit
+    (void)data; (void)id_out;
     return -1;
 }
 
